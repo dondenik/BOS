7fd56b181055ea29b204c8133faf0c6ea8d827e9
Ezra Shaw <ezrasure@outlook.com>
2023-03-31 13:56:46 +1300

    feat: implement 32bit and C kernel

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo "compiling..."
-nasm ./boot_sect.asm -f bin -o ./boot_sect.bin
-nasm ./kernel.asm -f bin -o ./kernel.bin
-cat ./boot_sect.bin ./kernel.bin > ./os-image
+echo "compiling boot files..."
+nasm ./boot/boot_sect.asm -f bin -o ./boot_sect.bin
+
+echo "compiling kernel"
+clang -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wno-main-return-type --target=i686-pc-none-elf -march=i686
+nasm kernel_entry.asm -f elf -o kernel_entry.o
+ld.lld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+
+cat boot_sect.bin kernel.bin > osimage.bin
 
 echo "running..."
-qemu-system-i386 -drive format=raw,file="./os-image"
+qemu-system-i386 -drive file="./osimage.bin",format=raw
