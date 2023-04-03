f155e90ba64ddfb7dd4243420df0de073b6f63e4
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-04-03 13:44:42 +1200

    fix: hardcode `lld` for dondenik

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ nasm ./boot/boot_sect.asm -f bin -o ./boot_sect.bin
 echo "compiling kernel"
 clang -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wno-main-return-type --target=i686-pc-none-elf -march=i686
 nasm kernel_entry.asm -f elf -o kernel_entry.o
-ld.lld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
-
+if [[ $(uname) == "Darwin" ]]; then
+    /opt/homebrew/Cellar/llvm/16.0.0/bin/ld.lld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+else
+    ld.lld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
+fi
 cat boot_sect.bin kernel.bin > osimage.bin
 
 echo "running..."
