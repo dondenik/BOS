1a7c9827e335673873f4300784a8c3ea437ffb11
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-04-05 12:19:00 +1200

    feat: clears screen on boot + added info about video memory

--- kernel.c
+++ kernel.c
@@ -1,4 +1,9 @@
 void main() {
   char *video_memory = (char *)0xb8000;
-  *video_memory = 'X';
+  // video memory is 80 x 25 cells (= 2000)
+  // video memory is made up of 16 bit cells
+  // the first 8 bits (one byte) is the actual character
+  // the last 8 bits (one byte) is the style information (colour, bold, etc)
+  // puts ' ' in every cell
+  for (int i = 0; i < 2000; i++) { *(video_memory + (i * 2)) = ' '; }
 }
